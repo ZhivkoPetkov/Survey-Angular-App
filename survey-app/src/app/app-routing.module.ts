97f293d3f67e86b7e980122f5201ecf51048013b
zhivkopetkov <azzurri@abv.bg>
2020-10-08 21:14:05 +0300

    Added categoryservice, category components

--- survey-app/src/app/app-routing.module.ts
+++ survey-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddCategoryComponent } from './components/add-category/add-category.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "add-category",
+    component: AddCategoryComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- survey-app/src/app/app.module.ts
+++ survey-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCategoryComponent } from './components/add-category/add-category.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCategoryComponent,
+    CategoryListComponent
   ],
   imports: [
     BrowserModule,
